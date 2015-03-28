e45c67c4f86ef72a7efb43aa8c4cc62f38a8c0b5
freinn@gmail.com <freinn@gmail.com>
2015-03-28 15:45:16 +0000

    otra kata, subo al nivel 7kyu

---
+++ ficheros_de_prueba/ArithmeticSequence.hs
@@ -0,0 +1,4 @@
+module ArithmeticSequence where
+
+nthterm :: Int -> Int -> Int -> Int
+nthterm first n c = first + n*c
--- ficheros_de_prueba/prueba1.hs
+++ ficheros_de_prueba/prueba1.hs
@@ -1,4 +1,5 @@
 import Data.List (take, takeWhile)
 import Data.Numbers.Primes (primes)
+import Data.List
 
 sumaPrimos = sum (takeWhile (< 2000000) primes)
