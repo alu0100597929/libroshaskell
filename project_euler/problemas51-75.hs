4f8ff918cb4c7f34457d0acee74f146ecabd95ec
freinn@gmail.com <freinn@gmail.com>
2015-04-07 18:49:39 +0100

    mejoras a la memoria, problema 52 resuelto

--- project_euler/problemas51-75.hs
+++ project_euler/problemas51-75.hs
@@ -1,3 +1,19 @@
+import Data.List
+
+{- problema 52 -}
+
+intALista :: Int -> [Int]
+intALista 0 = []
+intALista n = intALista (n `div` 10) ++ [n `mod` 10]
+
+esPermutacion :: Int -> Int -> Bool
+esPermutacion a b = (sort $ intALista a) == (sort $ intALista b)
+
+condiciones :: Int -> Bool
+condiciones x = and $ map (\y -> esPermutacion x $ x*y) [2..6]
+
+solucion52 = find (condiciones) [1..]
+
 {- problema 56 -}
 sumaCifras :: Integer -> Integer
 sumaCifras 0 = 0
