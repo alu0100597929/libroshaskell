03899893a9f8ba9375e560b1565fe87cc98efd3a
freinn <freinn@gmail.com>
2015-04-10 14:04:38 +0100

    hecho el tutorial de wikibooks sobre la mónada maybe

--- monadas/Wikibooks_Understanding_monads/maybe/MaybeMonad.hs
+++ monadas/Wikibooks_Understanding_monads/maybe/MaybeMonad.hs
@@ -1,3 +1,5 @@
+module MaybeMonad where
+
 import Prelude hiding (Maybe, Nothing, Just)
 
 data Maybe x = Nothing | Just x
@@ -9,16 +11,21 @@ Nothing :: Maybe a
 y el pattern matching
 -}
 
+-- fmap :: (Functor f) => (a -> b) -> f a -> f b
 instance Functor Maybe where
   fmap _ Nothing = Nothing
   fmap f (Just x) = Just (f x) 
 
+--pure :: a -> f a
+--(<*>) :: f (a -> b) -> f a -> f b
 instance Applicative Maybe where
   pure = Just
 
   Nothing <*> _ = Nothing
   (Just f) <*> v = fmap f v
 
+-- return :: a -> m a
+-- (>>=) :: m a -> (a -> m b) -> m b
 instance Monad Maybe where
   return a = Just a
 
