631f9df4c04615038d14a4a39a9d253829d0ccb8
freinn <freinn@gmail.com>
2015-04-22 08:38:42 +0100

    añadido script para arreglar el tearing en gráficas integradas intel

---
+++ intel-tearing-solution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#!!!First install mesa utilitis if u dont have it!!
+sudo apt-get install mesa-utils
+#To see what mesa and opengl version u have
+glxinfo | grep OpenGL
+sudo mkdir /etc/X11/xorg.conf.d/
+echo -e 'Section "Device"\n Identifier "Intel Graphics"\n Driver "Intel"\n Option "AccelMethod" "sna"\n Option "TearFree" "true"\nEndSection' \
+| sudo tee /etc/X11/xorg.conf.d/20-intel.conf
+sudo reboot
