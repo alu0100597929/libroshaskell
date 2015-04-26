9730c1ac919a9a69d5579aea93953d7cbff96302
freinn <freinn@gmail.com>
2015-04-26 19:30:10 +0100

    avances en el tutorial de parseo de JSON

--- write48/JSON_parser/JSON_parser.hs
+++ write48/JSON_parser/JSON_parser.hs
@@ -11,6 +11,9 @@ alwaysTrue = pure True
 matchTrue :: Parser String
 matchTrue = string "true"
 
+matchFalse :: Parser String
+matchFalse = string "false"
+
 -- ahora queremos combinar los dos tipos, y recibiendo una String,
 -- queremos devolver un Bool, necesitamos (*>) "star arrow"
 -- (*>) primero intenta lo de la izq., si tiene éxito, ejecuta lo de
@@ -22,9 +25,6 @@ matchTrue = string "true"
 boolTrue :: Parser Bool
 boolTrue = matchTrue *> alwaysTrue
 -- boolTrue = (string "true") *> (pure True)
-
-matchFalse :: Parser String
-matchFalse = string "false"
 
 boolFalse :: Parser Bool
 boolFalse = matchFalse *> alwaysTrue
@@ -47,4 +47,110 @@ stringLiteral =
 parse stringLiteral "test" "\"hello\""
 parse stringLiteral "test" "true"
 parse stringLiteral "test" "\"true\""
+-}
+
+-- la siguiente línea da error de tipos
+-- value = bool <|> stringLiteral
+-- solución: crear un tipo algebraico que contenga Bool y String
+
+data JSONValue = B Bool
+               | S String
+               | A [JSONValue] --array de JSON
+               | O [(String, JSONValue)]
+               deriving Show
+
+-- parser principal, usamos spaces al principio, porque es lo
+-- único que da fallo con los lexeme parsers, el principio con espacios.
+jsonValue :: Parser JSONValue
+jsonValue = spaces >> (jsonBool <|> jsonStringLiteral <|> jsonArray
+                           <|> jsonObject )
+
+-- pero esto también da error de tipos porque recordemos:
+-- bool :: Parser Bool
+-- stringLiteral :: Parser String
+-- solución: usar la clase de tipos Applicative
+-- ($) :: Functor f => (a -> b) -> f a -> f b
+-- ($) :: (a -> b) -> Parser a -> Parser b
+-- map :: (a -> b) -> [a] -> [b]
+
+jsonBool' :: Parser JSONValue
+jsonBool' = B <$> bool
+-- jsonBool = fmap B bool
+
+-- jsonStringLiteral :: Parser JSONValue
+-- jsonStringLiteral = S <$> stringLiteral
+-- jsonStringLiteral = fmap S stringLiteral
+
+jsonStringLiteral :: Parser JSONValue
+jsonStringLiteral = lexeme (S <$> stringLiteral)
+
+{-
+parse jsonBool "test" "true"
+parse jsonBool "test" "false"
+parse jsonBool "test" "lemon"
+-}
+
+{-
+jsonValue :: Parser JSONValue
+jsonValue = jsonBool <|> jsonStringLiteral
+-}
+
+{-
+parse jsonValue "test" "\"hello\""
+parse jsonValue "test" "true"
+-}
+
+array :: Parser [JSONValue]
+array =
+  (lexeme $ char '[')
+  *>
+  ( jsonValue `sepBy` (lexeme $ char ',') )
+  <*
+  (lexeme $ char ']')
+
+{-
+parse array "test" "[\"ola\",\"k\",\"ase\"]"
+Right [S "ola",S "k",S "ase"]
+-}
+
+jsonArray :: Parser JSONValue
+jsonArray = A <$> array
+
+jsonObject :: Parser JSONValue
+jsonObject = O <$> ((lexeme $ char '{') *>
+                    (objectEntry `sepBy` (lexeme $ char ','))
+                    <* (lexeme $ char '}'))
+
+objectEntry :: Parser (String, JSONValue)
+objectEntry = do 
+  key <- lexeme stringLiteral 
+  char ':'
+  value <- lexeme jsonValue
+  return (key, value)
+
+{-
+*Main> parse objectEntry "test" "\"beer\":true"
+Right ("beer",B True)
+*Main> parse objectEntry "test" "\"beer\":true"
+Right ("beer",B True)
+-}
+
+-- lidiemos con espacios intercalados usando lexeme
+
+ws :: Parser String
+ws = many (oneOf " \t\n")
+
+-- mi propio combinador lexeme
+lexeme :: Parser a -> Parser a
+lexeme p = p <* ws
+
+jsonBool = lexeme jsonBool'
+
+-- el resto de Parsers ya fueron arreglados para usar lexeme.
+-- es importante darse cuenta de que lexeme debe usarse en cada uno
+-- de los Parsers más simples
+
+{-
+*Main> parse jsonValue "test" "[true, true, true]"
+Right (A [B True,B True,B True])
 -}
