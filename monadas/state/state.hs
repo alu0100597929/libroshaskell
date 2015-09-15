77c9cffc41ed0511f2b320fb01a4746f860a3456
freinn <freinn@gmail.com>
2015-09-15 11:10:02 +0100

    borrada mónada estado para trabajar

--- monadas/state/state.hs
+++ monadas/state/state.hs
@@ -3,20 +3,3 @@
 import Prelude hiding ((>>=), return)
 
 newtype State s a = S (s -> (a, s))
-
-runState :: State s a -> s -> (a, s)
-runState (S f) s = f s
-
-return :: a -> State s a
-return a = S $ \s -> (a,  s)
-
-get :: State s s
-get = S $ \s -> (s, s)
-
-put :: s -> State s ()
-put s = S $ \_ -> ((), s)
-
-(>>=) :: State s a -> (a -> State s b) -> State s b
-m >>= k = S $ \s ->
-  let (a, s') = runState m s
-  in  runState (k a) s'
